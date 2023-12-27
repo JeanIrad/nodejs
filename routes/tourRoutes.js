c5d68dc65254810ebe793d142369845dafc504f2
Jean de Dieu <jado.milton@gmail.com>
2023-12-27 09:13:10 +0200

    some virtual changes applied

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 
 const tourController = require('../controllers/tourController');
-const { get } = require('mongoose');
+// const { get } = require('mongoose');
 const {
   getAllTours,
   createTour,
@@ -9,10 +9,14 @@ const {
   deleteTour,
   getTour,
   aliasTopTours,
+  getTourStats,
+  getMonthlyPlan,
 } = tourController;
 const router = express.Router();
 
 router.route('/top-5-cheap').get(aliasTopTours, getAllTours);
+router.route('/tour-stats').get(getTourStats);
+router.route('/monthly-plan/:year').get(getMonthlyPlan);
 
 router.route('/').get(getAllTours).post(createTour);
 router.route('/:id').get(getTour).patch(updateTour).delete(deleteTour);
